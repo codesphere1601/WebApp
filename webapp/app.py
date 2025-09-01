8989c2dd64d0d976b0c655df116251ac3505311f
Abhishek <sharma.abhishek.as07@gmail.com>
2025-09-02 00:29:56 +0530

    smtp

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,12 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+EMAIL = "info@codesphere.space"
+PASSWORD = "infoCodeSphere@123"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,10 +16,45 @@ def index():
         # fetch data from form
         user_data["name"] = request.form.get("name")
         user_data["email"] = request.form.get("email")
+        user_data["subject"] = request.form.get("subject")
         user_data["message"] = request.form.get("message")
 
-        # pass submitted data to success page
-        return render_template("success.html", data=user_data)
+        try:
+            # prepare the email
+            msg = MIMEMultipart()
+            msg["From"] = EMAIL
+            msg["To"] = request.form.get("email")  # change to your destination
+            msg["cc"] = "info@codesphere.space"
+            msg["Subject"] = request.form.get("subject")
+
+            user_body = f"""
+            Hi {user_data['name']},
+            {user_data['email']}
+
+            ✅ Thank you for contacting CodeSphere!  
+            We have received your query and our team will get back to you shortly.
+
+            Here’s a copy of your submission:
+            
+            Subject: {user_data['subject']}
+            Message: {user_data['message']}
+
+            Best regards,  
+            CodeSphere Team
+            """
+            msg.attach(MIMEText(user_body, "plain"))
+
+            # send via yahoo smtp
+            server = smtplib.SMTP_SSL("smtp.hostinger.com", 465)
+            server.login(EMAIL, PASSWORD)
+            server.sendmail(EMAIL, msg["To"], msg.as_string())
+            server.quit()
+
+            # success = True
+            return render_template("index.html", data={})
+
+        except Exception as e:
+            print("❌ Error sending email:", e)
 
     # initial load
     return render_template("index.html", data=user_data)
